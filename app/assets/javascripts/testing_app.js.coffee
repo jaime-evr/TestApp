af786d12dd6d8b13783c03e77c0b38a3003870e1
jaime <jaime.victoria.90@gmail.com>
2013-09-26 22:04:56 -0500

    project settup

---
+++ app/assets/javascripts/testing_app.js.coffee
@@ -0,0 +1,9 @@
+window.TestingApp =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  initialize: -> alert 'Hello from Backbone!'
+
+$(document).ready ->
+  TestingApp.initialize()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestingApp::Application.config.secret_key_base = '37d2407fee27b018d0b103385b7bc270ea2ac4408ae4630a430576a9fac83eba93b9cb6cdf7912a7c037639f3358fe60b71491ee962ca093cd1b5d01848ab740'
